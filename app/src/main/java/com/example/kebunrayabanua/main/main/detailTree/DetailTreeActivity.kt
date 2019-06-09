3222a7a27ecf8fa007645b49510c69c8238fc1fb
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2019-06-09 09:18:01 +0800

    Tree Data and Detail Completed

--- app/src/main/java/com/example/kebunrayabanua/main/main/detailTree/DetailTreeActivity.kt
+++ app/src/main/java/com/example/kebunrayabanua/main/main/detailTree/DetailTreeActivity.kt
@@ -2,16 +2,33 @@ package com.example.kebunrayabanua.main.main.detailTree
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.transition.Visibility
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.Glide
 import com.example.kebunrayabanua.R
 import kotlinx.android.synthetic.main.detail_tree_activity.*
+import kotlinx.android.synthetic.main.detail_tree_activity.appbar
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verbose
 
-class DetailTreeActivity : AppCompatActivity() {
+class DetailTreeActivity : AppCompatActivity(), AnkoLogger, AppBarLayout.OnOffsetChangedListener {
+
+    override fun onOffsetChanged(p0: AppBarLayout?, verticalOffset: Int) {
+        if(verticalOffset <= -55)
+            title_bar.visibility = View.VISIBLE
+        else
+            title_bar.visibility = View.GONE
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_tree_activity)
-        setSupportActionBar(toolbar)
-        collapsing_toolbar.title = "Eucalypus Aselole"
+        Glide.with(this).load(R.drawable.header_2).into(header_img)
+        appbar.addOnOffsetChangedListener(this)
     }
 
 }
